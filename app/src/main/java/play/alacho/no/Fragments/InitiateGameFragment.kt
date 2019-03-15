3ce4b3bf75d53c0e3232c34e821642aa8c5e5afe
Håvard Nordlie Mathisen <havard@alacho.no>
2019-03-15 11:55:46 +0100

    Working selector, minus one bug

--- app/src/main/java/play/alacho/no/Fragments/InitiateGameFragment.kt
+++ app/src/main/java/play/alacho/no/Fragments/InitiateGameFragment.kt
@@ -1,6 +1,7 @@
 package play.alacho.no.Fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,16 +11,20 @@ import kotlinx.android.synthetic.main.initiate_game_fragment.*
 import play.alacho.no.Game.Player
 import play.alacho.no.pgr202_tictactoe.R
 
-class InitiateGameFragment : Fragment(), View.OnClickListener{
+class InitiateGameFragment : Fragment(), View.OnClickListener {
 
   private var currentValue: String = ""
   private var playerOne: Player = Player()
   private var playerTwo: Player = Player()
   private var playerOneSelected: Boolean = false
+  private var playerTwoSelected: Boolean = false
 
+  companion object {
+    private const val PADDING_VALUE: Int = 30
+  }
 
-   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-     return inflater.inflate(R.layout.initiate_game_fragment, container, false)
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.initiate_game_fragment, container, false)
   }
 
   override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -31,58 +36,75 @@ class InitiateGameFragment : Fragment(), View.OnClickListener{
   }
 
   override fun onClick(v: View) {
-    when(v.id){
-      R.id.botSelector -> { botSelector() }
-      R.id.startGameBtn -> { beginGame() }
-      else -> { handleImageClick(v.tag.toString()) }
+    when (v.id) {
+      R.id.botSelector -> {
+        botSelector()
+      }
+      R.id.startGameBtn -> {
+        beginGame()
+      }
+      else -> {
+        handleImageClick(v.tag.toString())
+      }
     }
   }
 
-  private fun botSelector(){
+  private fun botSelector() {
+    Log.d("Inkyimage", inkyImage.paddingBottom.toString())
     if (botSelector.isChecked) {
       playerTwoNameInput.isEnabled = false
       currentValue = playerTwoNameInput.text.toString()
+      setImageValues("Inky", R.drawable.playertwo_button_border)
       playerTwoNameInput.setText(getString(R.string.botName))
+      playerTwoSelected = true
     } else {
       playerTwoNameInput.isEnabled = true
       playerTwoNameInput.setText(currentValue)
+      inkyImage.isEnabled = true
+      inkyImage.setBackgroundResource(0)
+      inkyImage.setPadding(0, 0, 0, 0)
+      playerTwoSelected = false
     }
   }
 
-  private fun handleImageClick(tag: String){
-    //Sett verdien til en variabel og clear den variabelen hver gang man velger
-    //Handle om resource allerede er valgt
+  /*
+    We need to save a reference to which image a player picked.
+    0 = Pacman
+    1 = Cherry
+    2 = Inky
+   */
 
-    var imageButton: ImageButton = pacmanImage
+  private fun handleImageClick(tag: String) {
 
-    //if(!playerOneSelected) {
-      when (tag) {
-        "Inky" -> { imageButton = inkyImage }
-        "Cherry" -> { imageButton = cherryImage }
-        "Pacman" -> { imageButton = pacmanImage }
-      }
-
-      with(imageButton){
-        setBackgroundResource(R.drawable.player_button_border)
-        isEnabled = false
-      }
-
+    if (!playerOneSelected) {
+      setImageValues(tag, R.drawable.player_button_border)
       playerOneSelected = true
+    } else if (!playerTwoSelected) {
+      setImageValues(tag, R.drawable.playertwo_button_border)
+      playerTwoSelected = true
+    }
 
-    /*} else {
-      when(tag) {
-        "Inky" -> {
-          inkyImage.setBackgroundResource(R.drawable.playertwo_button_border)
-        }
-        "Pacman" -> {
-          pacmanImage.setBackgroundResource(R.drawable.playertwo_button_border)
-        }
-      }
-    } */
+    else if (playerTwoSelected && playerTwoSelected){
+
+    }
+
+  }
+  private fun setImageValues(tag: String?, resource: Int){
+
+    var image: ImageButton? = null
+
+    when (tag) {
+      "Inky" -> { image = inkyImage }
+      "Cherry" -> { image = cherryImage }
+      "Pacman" -> { image = pacmanImage }
+    }
+    image?.setBackgroundResource(resource)
+    image?.setPadding(PADDING_VALUE, PADDING_VALUE, PADDING_VALUE, PADDING_VALUE)
+    image?.isEnabled = false
   }
 
   private fun beginGame() {
-
+    //Send the data to the next fragment which is the actual game
   }
 
 }
--- app/src/main/java/play/alacho/no/Game/Player.kt
+++ app/src/main/java/play/alacho/no/Game/Player.kt
@@ -1,7 +1,19 @@
 package play.alacho.no.Game
 
+import android.widget.ImageButton
+
 class Player {
 
+  private var name: String = ""
+  private var resource: Int = 0
+  private var image: ImageButton? = null
+
   constructor()
-  constructor(name: String, id: Int, resource: Int = 0)
+
+
+  constructor(name: String, image: ImageButton, resource: Int = 0){
+    this.name = name
+    this.image = image
+    this.resource = resource
+  }
 }

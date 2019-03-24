fbe3fc3108630daa577fa17bc34d5dba14abcb1c
Håvard Nordlie Mathisen <havard@alacho.no>
2019-03-24 16:26:14 +0100

    Mislogic on win

--- app/src/main/java/play/alacho/no/fragments/Game.kt
+++ app/src/main/java/play/alacho/no/fragments/Game.kt
@@ -23,7 +23,7 @@ class Game : FragmentHelper(), View.OnClickListener {
   private lateinit var playerTwo: Player
   private var winningList: List<List<Int>> =
     listOf( listOf(0,1,2), listOf(3,4,5), listOf(6,7,8), listOf(0,3,6),
-      listOf(1,4,5), listOf(2,5,8), listOf(0,4,8), listOf(2,4,6)
+      listOf(1,4,7), listOf(2,5,8), listOf(0,4,8), listOf(2,4,6)
     )
 
   override fun onCreate(savedInstanceState: Bundle?) {
